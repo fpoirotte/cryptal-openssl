faf4eda2fd22204bd31e3dfffcc37fa8af77a5ef
François Poirotte <clicky@erebot.net>
2017-07-28 16:43:48 +0200

    Convert the code to use the latest API

--- src/Openssl.php
+++ src/Openssl.php
@@ -1,47 +1,61 @@
 <?php
 
-namespace fpoirotte\Cryptal\Implementers;
+namespace fpoirotte\Cryptal\Plugins;
 
+use fpoirotte\Cryptal\Implementers\PluginInterface;
 use fpoirotte\Cryptal\Implementers\CryptoInterface;
+use fpoirotte\Cryptal\RegistryWrapper;
 use fpoirotte\Cryptal\PaddingInterface;
+use fpoirotte\Cryptal\CipherEnum;
+use fpoirotte\Cryptal\ModeEnum;
+use fpoirotte\Cryptal\ImplementationTypeEnum;
 
-class Crypto implements CryptoInterface
+class Openssl implements CryptoInterface, PluginInterface
 {
     protected $method;
     protected $tagLength;
     protected $padding;
     protected $aead;
+    protected $cipher;
+    protected $key;
 
     protected static $supportedCiphers = null;
     protected static $supportedModes = null;
 
-    public function __construct($cipher, $mode, PaddingInterface $padding, $tagLength = 16)
-    {
+    public function __construct(
+        CipherEnum          $cipher,
+        ModeEnum            $mode,
+        PaddingInterface    $padding,
+        $key,
+        $tagLength = self::DEFAULT_TAG_LENGTH
+    ) {
         if (static::$supportedCiphers === null) {
             static::checkSupport();
         }
 
-        if (!isset(static::$supportedCiphers[$cipher], static::$supportedModes[$mode])) {
+        if (!isset(static::$supportedCiphers["$cipher"], static::$supportedModes["$mode"])) {
             throw new \Exception('Unsupported cipher or mode');
         }
 
         $methods    = openssl_get_cipher_methods();
-        $method     = static::$supportedCiphers[$cipher] . '-' . static::$supportedModes[$mode];
+        $method     = static::$supportedCiphers["$cipher"] . '-' . static::$supportedModes["$mode"];
         if (!in_array($method, $methods)) {
             throw new \Exception('Unsupported cipher or mode');
         }
 
         $php71plus  = version_compare(PHP_VERSION, '7.1.0', '>=');
         $aead       = array(
-            CryptoInterface::MODE_CCM,
-            CryptoInterface::MODE_EAX,
-            CryptoInterface::MODE_GCM,
+            ModeEnum::MODE_CCM(),
+            ModeEnum::MODE_EAX(),
+            ModeEnum::MODE_GCM(),
         );
 
         $this->method       = $method;
         $this->tagLength    = $tagLength;
         $this->padding      = $padding;
         $this->aead         = $php71plus && in_array($mode, $aead);
+        $this->cipher       = $cipher;
+        $this->key          = $key;
     }
 
     protected static function checkSupport()
@@ -50,36 +64,36 @@ class Crypto implements CryptoInterface
         $ciphers        = array();
         $map            = array(
             'modes'     => array(
-                'cbc' => CryptoInterface::MODE_CBC,
-                'cfb' => CryptoInterface::MODE_CFB,
-                'ctr' => CryptoInterface::MODE_CTR,
-                'ecb' => CryptoInterface::MODE_ECB,
-                'ocb' => CryptoInterface::MODE_OCB,
-                'ofb' => CryptoInterface::MODE_OFB,
+                'cbc' => (string) ModeEnum::MODE_CBC(),
+                'cfb' => (string) ModeEnum::MODE_CFB(),
+                'ctr' => (string) ModeEnum::MODE_CTR(),
+                'ecb' => (string) ModeEnum::MODE_ECB(),
+                'ocb' => (string) ModeEnum::MODE_OCB(),
+                'ofb' => (string) ModeEnum::MODE_OFB(),
             ),
             'ciphers'   => array(
-                'des-ede3'      => CryptoInterface::CIPHER_3DES,
-                'aes-128'       => CryptoInterface::CIPHER_AES_128,
-                'aes-192'       => CryptoInterface::CIPHER_AES_192,
-                'aes-256'       => CryptoInterface::CIPHER_AES_256,
-                'bf'            => CryptoInterface::CIPHER_BLOWFISH,
-                'camelia-128'   => CryptoInterface::CIPHER_CAMELIA_128,
-                'camelia-192'   => CryptoInterface::CIPHER_CAMELIA_192,
-                'camelia-256'   => CryptoInterface::CIPHER_CAMELIA_256,
-                'cast5'         => CryptoInterface::CIPHER_CAST5,
-                'des'           => CryptoInterface::CIPHER_DES,
-                'rc2'           => CryptoInterface::CIPHER_RC2,
-                'rc4'           => CryptoInterface::CIPHER_RC4,
-                'seed'          => CryptoInterface::CIPHER_SEED,
+                'des-ede3'      => (string) CipherEnum::CIPHER_3DES(),
+                'aes-128'       => (string) CipherEnum::CIPHER_AES_128(),
+                'aes-192'       => (string) CipherEnum::CIPHER_AES_192(),
+                'aes-256'       => (string) CipherEnum::CIPHER_AES_256(),
+                'bf'            => (string) CipherEnum::CIPHER_BLOWFISH(),
+                'camelia-128'   => (string) CipherEnum::CIPHER_CAMELIA_128(),
+                'camelia-192'   => (string) CipherEnum::CIPHER_CAMELIA_192(),
+                'camelia-256'   => (string) CipherEnum::CIPHER_CAMELIA_256(),
+                'cast5'         => (string) CipherEnum::CIPHER_CAST5(),
+                'des'           => (string) CipherEnum::CIPHER_DES(),
+                'rc2'           => (string) CipherEnum::CIPHER_RC2(),
+                'rc4'           => (string) CipherEnum::CIPHER_RC4(),
+                'seed'          => (string) CipherEnum::CIPHER_SEED(),
             ),
         );
 
         // The API required to support AEAD was added in PHP 7.1.
         if (version_compare(PHP_VERSION, '7.1.0', '>=')) {
             $map['modes'] += array(
-                'ccm' => CryptoInterface::MODE_CCM,
-                'eax' => CryptoInterface::MODE_EAX,
-                'gcm' => CryptoInterface::MODE_GCM,
+                'ccm' => (string) ModeEnum::MODE_CCM(),
+                'eax' => (string) ModeEnum::MODE_EAX(),
+                'gcm' => (string) ModeEnum::MODE_GCM(),
             );
         }
 
@@ -95,28 +109,28 @@ class Crypto implements CryptoInterface
         static::$supportedCiphers   = array_flip(array_intersect_key($map['ciphers'], $ciphers));
     }
 
-    public function encrypt($iv, $key, $data, &$tag = null, $aad = '')
+    public function encrypt($iv, $data, &$tag = null, $aad = '')
     {
         $blockSize  = $this->getBlockSize();
         $missing    = $blockSize - (strlen($data) % $blockSize);
         $data      .= $this->padding->getPaddingData($blockSize, $missing);
         $options    = OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING;
         if ($this->aead) {
-            $res = openssl_encrypt($data, $this->method, $key, $options, $iv, $tag, $aad, $this->tagLength);
+            $res = openssl_encrypt($data, $this->method, $this->key, $options, $iv, $tag, $aad, $this->tagLength);
         } else {
-            $res = openssl_encrypt($data, $this->method, $key, $options, $iv);
+            $res = openssl_encrypt($data, $this->method, $this->key, $options, $iv);
         }
         return $res;
     }
 
-    public function decrypt($iv, $key, $data, $tag = null, $aad = '')
+    public function decrypt($iv, $data, $tag = null, $aad = '')
     {
         $blockSize  = $this->getBlockSize();
         $options    = OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING;
         if ($this->aead) {
-            $res = openssl_decrypt($data, $this->method, $key, $options, $iv, $tag, $aad);
+            $res = openssl_decrypt($data, $this->method, $this->key, $options, $iv, $tag, $aad);
         } else {
-            $res = openssl_decrypt($data, $this->method, $key, $options, $iv);
+            $res = openssl_decrypt($data, $this->method, $this->key, $options, $iv);
         }
         $padLen     = $this->padding->getPaddingSize($res, $blockSize);
         return $padLen ? (string) substr($res, 0, -$padLen) : $res;
@@ -155,5 +169,30 @@ class Crypto implements CryptoInterface
         }
 
         return strlen($res);
+    }
+
+    public static function registerAlgorithms(RegistryWrapper $registry)
+    {
+        static::checkSupport();
+        foreach (static::$supportedModes as $mode => $modeConst) {
+            foreach (static::$supportedCiphers as $cipher => $cipherConst) {
+                $registry->addCipher(
+                    __CLASS__,
+                    CipherEnum::$cipher(),
+                    ModeEnum::$mode(),
+                    ImplementationTypeEnum::TYPE_COMPILED()
+                );
+            }
+        }
+    }
+
+    public function getCipher()
+    {
+        return $this->cipher;
+    }
+
+    public function getKey()
+    {
+        return $this->key;
     }
 }
